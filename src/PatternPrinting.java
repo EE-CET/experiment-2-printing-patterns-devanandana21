3e88cc5913f45e456988662c413bd0ed5345738e
devanandana21 <tve24el018@cet.ac.in>
2026-01-14 22:25:13 +0530

    Update PatternPrinting.java

--- src/PatternPrinting.java
+++ src/PatternPrinting.java
@@ -1,5 +1,20 @@
 import java.util.Scanner;
 
 public class PatternPrinting {
-    
+     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter n: ");
+        int n = sc.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            System.out.println("*");
+        }
+
+        for (int i = 0; i < n; i++) {
+            System.out.print("*");
+        }
+
+        sc.close();
+    }
 }
